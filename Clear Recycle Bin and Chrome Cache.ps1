0b8d6af602e47c3afa3139ec409a2aacd1a55b83
Kylebrody <36281177+Kylebrody@users.noreply.github.com>
2023-05-03 20:46:29 -0500

    Text app to clear recycle bin and chrome cache
    Gives you the option to do both with one tool

---
+++ Clear Recycle Bin and Chrome Cache.ps1
@@ -0,0 +1,32 @@
+﻿Get-ChildItem C:\Users\kyles\Downloads\* | Remove-Item -recurse -force
+Clear-RecycleBin
+Write-Host " "
+Write-Host "Downloads and Recycle Bin Cleared"
+Write-Host " "
+
+$HistChoice = Read-Host -Prompt "Would you also like to clear your Chrome browsing data? This will stop Chrome if it is currently running.Y/N?"
+
+if ($HistChoice -eq 'Y') {
+    taskkill /F /IM "chrome.exe" /fi "memusage gt 2"
+    Start-Sleep -Seconds 1
+    $Items = @('Archived History',
+                'Cache\*',
+                'Cookies',
+                'History',
+                'Login Data',
+                'Top Sites',
+                'Visited Links',
+                'Web Data')
+    $Folder = "$($env:LOCALAPPDATA)\Google\Chrome\User Data\Default"
+    $Items | % { 
+        if (Test-Path "$Folder\$_") {
+            Remove-Item "$Folder\$_" -Recurse
+        }
+    }
+    Write-Host "Chrome Data Cleared"
+}
+
+
+Write-Host All clean baby!!
+
+pause
