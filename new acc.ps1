f19fb27f52c55cc7874d11e1ff2d17193127ad92
Kylebrody <36281177+Kylebrody@users.noreply.github.com>
2023-05-03 20:42:15 -0500

    Adding new account ps1 script
    Creates a new windows user and logs out of your existing account automatically

---
+++ new acc.ps1
@@ -0,0 +1,6 @@
+﻿$Password = Read-Host -AsSecureString
+New-LocalUser "test" -Password $Password -FullName "test" -Description "Description of this account."
+Add-LocalGroupMember -Group "Administrators" -Member "test"
+#Start-Process -FilePath C:\Users\kyles\Downloads\AutoLogon\Autologon64.exe
+#pause
+#Restart-Computer -Force
